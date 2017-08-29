21c50efd752f29933fce95309473dedf144e59a3
Gorobets Dmitry <dgorobets@users.noreply.github.com>
2017-08-29 13:42:05 +0500

    Update urls.py

--- robokassa/urls.py
+++ robokassa/urls.py
@@ -1,8 +1,8 @@
 #coding: utf-8
 try:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
 except ImportError:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 
 from robokassa import views
     
